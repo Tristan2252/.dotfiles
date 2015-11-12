18cd9892c20c21f31f31c8f9d3912075de74a8d9
Tristan2252 <tvigil25@live.com>
2015-11-11 21:27:44 -0700

    vim added

---
+++ .vim/setup.sh
@@ -0,0 +1,25 @@
+#!/bin/bash
+
+# Seting up plugin dir's
+mkdir -p ~/.vim/autoload ~/.vim/bundle
+
+# install pathogen
+curl -LSso ~/.vim/autoload/pathogen.vim https://tpo.pe/pathogen.vim
+
+# supertab "dont forget to source with 'source %' command"
+git clone https://github.com/ervandew/supertab.git ~/.vim/bundle/supertab
+echo ""
+echo "Dont forget to source SUPERTAB with :so % command"
+echo ""
+sleep 2
+
+# syntastic
+git clone https://github.com/scrooloose/syntastic.git ~/.vim/bundle/syntastic
+
+# Auto Pairs
+git clone git://github.com/jiangmiao/auto-pairs.git ~/.vim/bundle/auto-pairs
+
+# The NERD Tree
+git clone https://github.com/scrooloose/nerdtree.git ~/.vim/bundle/nerdtree
+
+exit

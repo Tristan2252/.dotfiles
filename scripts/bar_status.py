ee9052421204e6105e78098b60731858b4658c9a
Tristan2252 <tvigil25@live.com>
2017-10-25 18:51:43 -0600

    moved seperators into functions for better color mgt

--- scripts/bar_status.py
+++ scripts/bar_status.py
@@ -10,10 +10,6 @@ def make_json(string, bgcolor, fgcolor):
            """' foreground='#""" + fgcolor + """' size='10800' font='FontAwesome 10'> """ + string + \
            """ </span>", "markup": "pango", "separator": false, "separator_block_width": -8}"""
 
-#def separator(color):
-#    return """{ "full_text": "<span foreground='#""" + color + """' size='11000' font='FontAwesome 10'></span>",""" + \
-#           """ "markup": "pango", "separator":false, "separator_block_width": -6 }"""
-
 def separator(color):
     return """{ "full_text": "<span foreground='#""" + color + """' size='10800'></span>",""" + \
            """ "markup": "pango", "separator":false, "separator_block_width": 0 }"""
@@ -25,22 +21,22 @@ def dividor(color):
 def status_net():
     output = os.popen("conky -i 1").read().rstrip()
     string = " WAN:    " + output.split(", ")[0] + "    "
-    return make_json(string, "002b36", "859900")
+    return separator("002b36") + "," + make_json(string, "002b36", "859900")
 
 def status_cpu():
     output = os.popen("conky -i 1").read().rstrip()
     string = "CPU:    " + output.split(", ")[1] + "    "
-    return make_json(string, "b58900", "002b36")
+    return separator("b58900") + "," + make_json(string, "b58900", "002b36")
 
 def status_ram():
     output = os.popen("conky -i 1").read().rstrip()
     string = "RAM:  " + output.split(", ")[2] + "    "
-    return make_json(string, "b58900", "002b36")
+    return dividor("b58900") + "," + make_json(string, "b58900", "002b36")
 
 def status_disk():
     output = os.popen("conky -i 1").read().rstrip()
     string = "HDD: " + output.split(", ")[3] + "    "
-    return make_json(string, "b58900", "002b36")
+    return dividor("b58900") + "," + make_json(string, "b58900", "002b36")
 
 def status_sound():
     output = os.popen("pactl list sinks | grep Mute | tail -1").read().rstrip()
@@ -60,13 +56,13 @@ def status_date():
     date_num = os.popen("date +'%m/%d/%Y'").read().rstrip()
     
     string = "   " + day + " " + date_num + "   "
-    return make_json(string, "859900", "002b36")
+    return separator("859900") + "," + make_json(string, "859900", "002b36")
 
 def status_time():
     time = os.popen("date +'%I:%M:%S'").read().rstrip()
     
     string  = "  " + time + "   "
-    return make_json(string, "859900", "002b36")
+    return dividor("859900") + "," + make_json(string, "859900", "002b36")
 
 def status_bat():
     
@@ -77,7 +73,7 @@ def status_bat():
         
     if state == "Charging,":
         string = "BAT:   {}".format(percent)
-        return make_json(string + status_sound(), "b3b2b2", "002b36")
+        return separator("b3b2b2") + "," + make_json(string + status_sound(), "b3b2b2", "002b36")
 
     if level <= 100 and level >= 80:
         string = "BAT:   {}".format(percent)
@@ -90,9 +86,9 @@ def status_bat():
     else: 
         BAT_COLOR = "cb4b16"
         string = "BAT:   {}".format(percent)
-        return make_json(string + status_sound(), "cb4b16", "002b36")
+        return separator("cb4b16") + "," + make_json(string + status_sound(), "cb4b16", "002b36")
 
-    return make_json(string + status_sound(), "b3b2b2", "002b36")
+    return separator("b3b2b2") + "," + make_json(string + status_sound(), "b3b2b2", "002b36")
 
 
 
@@ -101,19 +97,12 @@ def main():
     
     # needs to be one single write to stdout because of i3status but 
     sys.stdout.write("[\n" + 
-            "{},\n".format(separator("b58900")) + 
             "{},\n".format(status_cpu()) + 
-            "{},\n".format(dividor("b58900")) + 
             "{},\n".format(status_ram()) + 
-            "{},\n".format(dividor("b58900")) + 
             "{},\n".format(status_disk()) + 
-            "{},\n".format(separator("002b36")) + 
             "{},\n".format(status_net()) + 
-            "{},\n".format(separator("859900")) + 
             "{},\n".format(status_date()) + 
-            "{},\n".format(dividor("859900")) + 
             "{},\n".format(status_time()) + 
-            "{},\n".format(separator(BAT_COLOR)) + 
             "{}\n".format(status_bat()) + 
             "],\n")
 

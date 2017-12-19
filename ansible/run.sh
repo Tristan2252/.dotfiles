e0520a6a3d4df7568c1eb4b932aee9b3f738e7bf
Tristan2252 <tvigil25@live.com>
2017-12-18 20:08:42 -0700

    practical fixes

--- ansible/run.sh
+++ ansible/run.sh
@@ -2,5 +2,11 @@
 
 if [ -e /usr/bin/ansible-playbook ]; then
     sudo ansible-playbook -i hosts i3laptop.yml
+    sudo rm -r $HOME/.ansible # remove ansible config after to prvent perm conflicts
+
     ansible-playbook -i hosts basic.yml
+    rm -r $HOME/.ansible # remove ansible dir after just cuz i dont need it
+    exit 0
 fi
+
+echo "Install Ansible to Run with 'sudo apt install ansible'"

3f45627ed0afa3db69aa12840dd9d42326154d0b
Tristan2252 <tvigil25@live.com>
2018-03-06 11:01:01 -0700

    added gesture setup to ansible and small edit to scrips

--- ansible/run.sh
+++ ansible/run.sh
@@ -2,10 +2,14 @@
 
 if [ -e /usr/bin/ansible-playbook ]; then
     sudo ansible-playbook -i hosts applications.yml
-    sudo rm -r $HOME/.ansible # remove ansible config after to prvent perm conflicts
+    if [ -e $HOME/.ansible ]; then 
+        sudo rm -r $HOME/.ansible # remove ansible config after to prvent perm conflicts
+    fi
 
-    ansible-playbook -i hosts configs.yml
-    rm -r $HOME/.ansible # remove ansible dir after just cuz i dont need it
+    ansible-playbook -i hosts configs.yml --tags "link,vim,lock,scripts,rc,cups,terminal,net_timeout,gesture"
+    if [ -e $HOME/.ansible ]; then 
+        rm -r $HOME/.ansible # remove ansible dir after just cuz i dont need it
+    fi
     exit 0
 fi
 
--- scripts/de_connect.sh
+++ scripts/de_connect.sh
@@ -1,18 +1,18 @@
 #!/bin/sh
 CONTAINER=CSE-326
-STATE=$(lxc info CSE-326 | grep -o Running)
+STATE=$(lxc info $CONTAINER | grep -o Running)
 
 if [ "$STATE" = "Running" ]; then 
-    lxc exec CSE-326 -- sudo --login --user ubuntu -i env DISPLAY=$DISPLAY bash
+    lxc exec $CONTAINER -- sudo --login --user ubuntu -i env DISPLAY=$DISPLAY bash
     exit 0
 fi
 
-lxc start CSE-326
+lxc start $CONTAINER
 
 echo "Connecting..."
 sleep 2
 
-lxc exec CSE-326 -- sudo --login --user ubuntu -i env DISPLAY=$DISPLAY bash
+lxc exec $CONTAINER -- sudo --login --user ubuntu -i env DISPLAY=$DISPLAY bash
 
 
 exit 0

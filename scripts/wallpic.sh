0a5724e0f22d44f94bbac1b67f11e26af2a2fe30
Tristan2252 <tvigil25@live.com>
2018-03-30 18:28:15 -0600

    new ansible installer

--- scripts/wallpic.sh
+++
@@ -1,9 +0,0 @@
-#! /bin/bash
-
-HOME=/home/tristan
-ALBUM=/home/tristan/Pictures/Wallpapers
-RAND_PIC=$(ls $ALBUM/ | sort --random-sort | head -1)
-
-rm $HOME/.wallpaper.jpg
-# copy random wallpaper to home for feh to read at login
-cp $ALBUM/$RAND_PIC $HOME/.wallpaper.jpg

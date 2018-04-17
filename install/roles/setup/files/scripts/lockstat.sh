6b53bab7676307fa69438cb8803173276adb5c26
Tristan2252 <tvigil25@live.com>
2018-04-17 09:31:39 -0600

    added unfocused window colors to i3

--- install/roles/setup/files/scripts/lockstat.sh
+++ install/roles/setup/files/scripts/lockstat.sh
@@ -1,7 +1,6 @@
 #! /bin/bash
 
 i3lock -i /home/tristan/.lockpic.png -c '#000000' -o '#191d0f' -w '#572020' -l '#ffffff' -e
-#i3lock -c '#000000' -o '#191d0f' -w '#572020' -l '#ffffff' -e
 
 #sleep 1
 
@@ -11,9 +10,6 @@ if [ -z "$LOCK_WIN_ID" ]; then
     exit 0
 fi
 
-#conky -q --window-id=$WIN_ID -c /home/tristan/.dotfiles/configs/conky_widget  &
 conky -q --window-id=$WIN_ID -c /home/tristan/.dotfiles/configs/conky_widget_bar  &
-#conky -q --window-id=$WIN_ID -c /home/tristan/.dotfiles/configs/conky_widget_bar_right  &
-#conky -q --window-id=$WIN_ID -c /home/tristan/.dotfiles/configs/conky_widget_left_middle &
 
 exit 0

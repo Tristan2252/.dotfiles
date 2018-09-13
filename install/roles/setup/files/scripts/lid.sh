3335f19e5c34386b49a0b32ed22c1d19ef65345d
Tristan2252 <tvigil25@live.com>
2018-09-13 11:45:15 -0600

    updated lid script to be more smart

--- install/roles/setup/files/scripts/lid.sh
+++ install/roles/setup/files/scripts/lid.sh
@@ -5,38 +5,74 @@
 # (see /etc/acpi and configs/lm_lid)
 
 INTERFACE=wlp3s0
-
 NET_STATE=$(cat /sys/class/net/$INTERFACE/operstate)
+SUSPEND_STATE=$(cat /proc/acpi/button/lid/*/state | awk '{print $2}')
 
-cat /proc/acpi/button/lid/*/state | grep -q close
-SUSPEND_STATE=$?
+logger "[Lid]: STARTED: NET_STATE ${NET_STATE} SUSPEND_STATE ${SUSPEND_STATE}"
 
-if [ $SUSPEND_STATE -eq 0 ]; then 
+get_best_net() {
+    if [ "${NET_STATE}" = "down"  ]; then
+        ip link set wlp3s0 up
+        sleep 2
+    fi
+
+    NETWORKS=($(iwlist ${INTERFACE} scan | grep -e "ESSID" -e "Quality" | sed 's/.*Quality=//g; s/Signal.*//g; s/.*ESSID://g'))
+
+     for network in ${!NETWORKS[@]}; do
+        if [ "${NETWORKS[${network}]}" = '"CSDeptWifi"' ]; then
+            INTERFACE="$INTERFACE=cs"
+            return
+        elif [ "${NETWORKS[${network}]}" = '"NMT-Encrypted"' ]; then
+            INTERFACE="$INTERFACE=nmt"
+            return
+        fi
+     done
+}
+
+if [ "${SUSPEND_STATE}" = "closed" ]; then 
     
     # set to allow for proper suspend
     if [ "$(cat /proc/acpi/wakeup | grep XHC1 | awk '{print $3}')" = "*enabled" ]; then 
         echo XHC1 > /proc/acpi/wakeup
+        logger "[Lid]: XHC1 DISABLED"
     fi
     if [ "$(cat /proc/acpi/wakeup | grep LID0 | awk '{print $3}')" = "*enabled" ]; then 
         echo LID0 > /proc/acpi/wakeup
+        logger "[Lid]: LID0 DISABLED"
     fi
     
     # if network is up, put down 
-    if [ $NET_STATE = "up"  ]; then
-        ifdown $INTERFACE
-        rfkill block wifi # block wifi
+    if [ "${NET_STATE}" = "up"  ]; then
+        logger "[Lid]: SETTING ${INTERFACE} => DOWN"
+        ifdown ${INTERFACE}
+        rfkill block wifi
     fi
 
+    logger "[Lid]: SUSPENDING..."
     systemctl suspend
-    logger "going to sleep"
-    exit 0
 fi
 
-#if [ $NET_STATE = "down"  ]; then
+if [ "${SUSPEND_STATE}" = "open" ]; then
+    sleep 2
+    
+    rfkill unblock wifi
+    
+    if [ $(pgrep dhclient) ]; then
+        logger "[Lid]: KILLING dhclient for ${INTERFACE}"
+        killall -9 dhclient
+    fi
+    if [ $(pgrep wpa_supplicant) ]; then
+        logger "[Lid]: KILLING wpa_supplicant for ${INTERFACE}"
+        killall -9 dhclient
+        ifdown ${INTERFACE}
+    fi
 
-sleep 2
-rfkill unblock wifi
-ifup $INTERFACE
+    get_best_net
 
-logger "done waking up"
+    logger "[Lid]: SETTING ${INTERFACE} => UP"
+    ifup $INTERFACE
+fi
+
+logger "[Lid]: STOPPED: NET_STATE ${NET_STATE} SUSPEND_STATE ${SUSPEND_STATE}"
+
 exit 0

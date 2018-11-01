287b27d017a13dd9615040f42fef97631fbbe0fb
Tristan2252 <tvigil25@live.com>
2018-10-31 18:15:52 -0600

    added changes to vimrc and more overlays for lock screen

--- install/roles/setup/files/scripts/lockpic.sh
+++ install/roles/setup/files/scripts/lockpic.sh
@@ -1,11 +1,11 @@
 #! /bin/bash
 
-HOME=/home/tristan
 ALBUM=/home/tristan/Pictures/Wallpapers
+OVERLAYS=~/.dotfiles/files
 RAND_PIC=$(ls $ALBUM/ | sort --random-sort | head -1)
-OVERLAY=~/.dotfiles/files/overlay2.png
+RAND_OVERLAY=$(ls $OVERLAYS/*.png | sort --random-sort | head -1)
 
 
 # covert random pic to home dir and resize for i3lock to read
 convert "$ALBUM/${RAND_PIC%.*}.jpg" -resize 2560x1600! $HOME/.lockpic.png
-composite -gravity center $OVERLAY $HOME/.lockpic.png $HOME/.lockpic.png
+composite -gravity center $RAND_OVERLAY $HOME/.lockpic.png $HOME/.lockpic.png

f247ce4895645b0e1acaaf7b02519392920d7dd6
Tristan2252 <tvigil25@live.com>
2019-03-11 20:56:26 -0600

    added se linux config for lockpic

--- install/roles/setup/files/scripts/lockpic.sh
+++ install/roles/setup/files/scripts/lockpic.sh
@@ -9,4 +9,4 @@ RAND_OVERLAY=$(ls $OVERLAYS/*.png | sort --random-sort | head -1)
 
 # covert random pic to home dir and resize for i3lock to read
 convert "$ALBUM/${RAND_PIC%.*}.jpg" -resize $SCREEN_SIZE! /home/tristan/.lockpic.png
-composite -gravity center $RAND_OVERLAY /home/tristan/.lockpic.png /home/tristan/.lockpic.png
+#composite -gravity center $RAND_OVERLAY /home/tristan/.lockpic.png /home/tristan/.lockpic.png
